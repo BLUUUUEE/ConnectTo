bb30841799e7d4d494104fed9eafcbe5487e52b2
BLUUUUEE <137717684+BLUUUUEE@users.noreply.github.com>
2024-07-02 16:30:54 +0530

    Completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,8 +21,8 @@ const hashedPassword = await bcrypt.hash(password,salt);
 
 
 
-const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=[${username}]`;
+const girlProfilePic =`https://avatar.iran.liara.run/public/girl?username=[${username}]`;
 
 const newUser = new User({
     fullName,
@@ -58,10 +58,10 @@ export const login = async (req,res)=>{
     const {username,password} = req.body;
     const user = await User.findOne({username});
 
-    const isPasswordCorrect = await bcrypt.compare(password,user.password || "");
-    if(!user || !isPasswordCorrect){
-        return res.status(400).json({error:"Invalid usrname or password"});
+    const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
 
+    if(!user || !isPasswordCorrect){
+        return res.status(401).json({error:"Invalid Username or Password"});
     }
 
     generateTokenAndSetCookie(user._id,res);
@@ -73,7 +73,7 @@ export const login = async (req,res)=>{
         profilePic: user.profilePic,
     });
  } catch (error) {
-    console.log("Error in login controller")
+    console.log("Error in login controller",error.message)
     res.status(500).json({error:"Internal server Error"});
  }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
 import Home from './pages/Home/Home'
+import { Routes ,Route,Navigate} from 'react-router-dom'
+import Login from "./pages/login/Login"
+import Signup from "./pages/signup/Signup"
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
   
-
-  return (
+const {authUser} = useAuthContext(); 
+return (
     <>
    <div className=' p-4 h-screen flex items-center justify-center'>
+   <Routes>
+   <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
    
-    <Home/>
+
+    
+   </Routes>
+   <Toaster />
+   
+   
    </div>
     </>
   )
 }
+
 
 export default App

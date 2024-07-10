f53c30d6d9eeda3bf05e5e8518b4b43c104ca8ec
BLUUUUEE <137717684+BLUUUUEE@users.noreply.github.com>
2024-07-10 18:39:25 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -10,7 +11,7 @@ import { app,server } from "./socket/socket.js";
 
 dotenv.config();
 
-
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontent","dist","index.html"));
+})
 
  // to parse incoming req with json payloads
 
